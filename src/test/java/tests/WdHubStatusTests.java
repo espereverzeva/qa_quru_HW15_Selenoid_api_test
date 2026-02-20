1c73edb411f56d6c86525d96fb1112d37b37afd2
Your Name <you@example.com>
2026-02-20 16:40:46 +0700

    added tests

---
+++ src/test/java/tests/WdHubStatusTests.java
@@ -0,0 +1,83 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.is;
+
+public class WdHubStatusTests extends TestBase {
+    @Test
+    public void bestTotalAmountTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void unauthorizedStatusTest() {
+        given()
+                .log().all()
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void successContentMessageTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("value.message", is("Selenoid 1.11.3 built at 2024-05-25_12:34:40PM"));
+    }
+
+    @Test
+    public void successContentReadyTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+
+    @Test
+    public void successSchemaTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/wd_hub_status_response_schema.json"));
+    }
+
+    @Test
+    public void negativeUnauthorizedPasswordTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "12345")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+}
